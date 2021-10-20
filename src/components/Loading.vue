5c67366d7f97ab767f81b259ee744d4dd8943234
Xuejian <beimengtianxia@gmail.com>
2021-10-21 01:40:28 +0800

    start

---
+++ src/components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="loading-page">
+    <div>加载中...</div>
+  </div>
+</template>
+<script>
+export default {
+  name: "loading",
+}
+</script>
+
+<style lang="scss" scoped>
+.loading-page {
+  div {
+    color: red;
+  }
+}
+</style>
